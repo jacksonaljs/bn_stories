a1808659823e8fd04f16fabf4d94ee03ec4647e6
Jackson Chelote <jacksonaljs@gmail.com>
2020-05-11 15:05:06 +0530

    Added trending and animated

--- lib/story.dart
+++ lib/story.dart
@@ -16,9 +16,7 @@ class _StoryState extends State<Story>{
           Column(
             children: <Widget>[
               _buildNavigationBar(),
-              Container(
-                child: Placeholder(),
-              )
+              _buildTrending(),
             ],
           )
         ],
@@ -26,7 +24,48 @@ class _StoryState extends State<Story>{
     );
   }
 
-  
+  _buildTrending(){
+    return Container(
+      padding: EdgeInsets.only(left: 30,right: 30,top: 10),
+      child: Row(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Trending",style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white
+              )),
+              SizedBox(
+                height: 10,),
+
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 30,vertical: 5),
+                decoration: ShapeDecoration(
+                  color: Color.fromARGB(255, 248, 112, 111),
+                  shape: StadiumBorder(),
+                ),
+                child: Text("Animated",style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600
+                ),),
+              )
+            ],
+          ),
+          Spacer(),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.white,
+              size: 40,
+            ))
+  ],
+    ),
+    );}
+
   _buildNavigationBar(){
     return SafeArea(
        bottom: false,
